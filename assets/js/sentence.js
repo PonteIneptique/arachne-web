73b13708dca51989675f0b87f5a4ab02596dbc92
Thibault Clérice <leponteineptique@gmail.com>
2014-03-06 12:32:11 +0000

    Generation of list of lemma connected to a form and vote display

--- assets/js/sentence.js
+++ assets/js/sentence.js
@@ -1,22 +1,89 @@
 $(document).ready(function() {
 
-        $('a.sentence-lemma').on("click", function(e) { e.preventDefault(); }).popover({
-        	html: true,
-        	placement : "bottom",
-        	content : function() {
-        		return $("#popover").html();
-        	}
-        });
-        $(".sentence-text").on("click", ".popover-category-title", function(e) {
-        	console.log($(this).parent())
-        	e.preventDefault();
-        	$(".popover-category-form").hide();
-        	$(this).parent().parent().parent().find(".popover-category-form").show();
-        });
-        $(".sentence-text").on("click", ".popover-title", function(e) {
-        	console.log("Here");
-        	$(".popover-category-form").hide();
-        	$(".popover-category-lemmas").show();
-        });
+	$('a.sentence-lemma').
+	on("click", function(e) { 
+		e.preventDefault(); 
+	}).
+	popover({
+		html: true,
+		placement : "bottom",
+		content : function() {
+
+			popover = $("#popover");
+			$(this).trigger("json");
+			return popover.html();
+		}
+	});
+
+	$(".sentence-text").on("json", "a.sentence-lemma", function(e) {
+
+		//Attributes
+		that = $(this)
+		form = that.attr("data-id");
+		sentence = $(".sentence-text").attr("data-id");
+		url = "/API/sentence/" + sentence + "/form/" + form;
+
+		var json = $.getJSON(url, function(data) {
+			$.each(data, function(i, item) {
+				vote = parseInt(item["votes"]);
+				if (i === 0) {
+					cl = " active ";
+				} else {
+					cl = "";
+				}
+				lemma = $( "<ul/>", {
+					"class": "nav nav-pills nav-ehri nav-ehri-grey nav-justified" + cl
+				}).append(
+					$("<li/>").append(
+						$("<div />", {
+							"class" : "row"
+						}).append(
+							$("<div/>", {
+								"class" : "col-xs-6",
+								html : "<a class='lemma' data-id='" + item["id_lemma"] + "'>" + item["text_lemma"] + "</a>"
+							})
+						).append(
+							$("<div/>", {
+								"class" : "col-xs-3",
+								html : function() {
+									if( vote > 0 ) {
+										return ' <a href="#" class="thumbs-up">' + vote + ' <span class="glyphicon glyphicon-thumbs-up"></span></a> ';
+									} else {
+										return ' <a href="#" class="thumbs-up">0 <span class="glyphicon glyphicon-thumbs-up"></span></a> ';
+									}
+								}
+							})
+						).append(
+							$("<div/>", {
+								"class" : "col-xs-3",
+								html : function() {
+									if( vote <= 0 ) {
+										return ' <a href="#" class="thumbs-down">' + vote + ' <span class="glyphicon glyphicon-thumbs-down"></span></a> ';
+									} else {
+										return ' <a href="#" class="thumbs-down">0 <span class="glyphicon glyphicon-thumbs-down"></span></a> ';
+									}
+								}
+							})
+						)
+					)
+				);
+				console.log(item["text_lemma"])
+				console.log(that.find(".append-in"));
+				that.next(".popover").find(".append-in").append(lemma);
+			}); //End each
+		});
+	})
+
+	$(".sentence-text").on("click", ".popover-category-title", function(e) {
+	console.log($(this).parent())
+		e.preventDefault();
+		$(".popover-category-form").hide();
+		$(this).parent().parent().parent().find(".popover-category-form").show();
+	});
+
+	$(".sentence-text").on("click", ".popover-title", function(e) {
+		$(".popover-category-form").hide();
+		$(".popover-category-lemmas").show();
+	});
 
 });
--- templates/pages/sentence.php
+++ templates/pages/sentence.php
@@ -3,48 +3,16 @@
 <a href="#" class="sentence-link">Link</a>
 
 
-<p class="sentence-text">
-	<?php
-		echo $sentence["processed"];
-	?>
+<p class="sentence-text" data-id="<?=$sentence["uid"];?>">
+	<?=$sentence["processed"];?>
 </p>
 
 
 
 <div id="popover" style="display:none;">
 	<div class="popover-category popover-category-form popover-category-lemmas">
-		<ul class="nav nav-pills nav-ehri nav-ehri-grey nav-justified">
-			<li>
-				<div class="row">
-					<div class="col-xs-6">
-						<a href="#">Mollus</a>
-					</div>
-					<div class="col-xs-3">
-						<a href="#" class="thumbs-up">1 <span class="glyphicon glyphicon-thumbs-up"></span></a>
-					</div>
-					<div class="col-xs-3">
-						<a href="#" class="thumbs-down">0 <span class="glyphicon glyphicon-thumbs-down"></span></a>
-					</div>
-
-				</div>
-			</li>
-		</ul>
-		<ul class="nav nav-pills nav-ehri nav-ehri-grey nav-justified">
-			<li>
-				<div class="row">
-					<div class="col-xs-6">
-						<a href="#">Mollis</a>
-					</div>
-					<div class="col-xs-3">
-						<a href="#" class="thumbs-up">2 <span class="glyphicon glyphicon-thumbs-up"></span></a>
-					</div>
-					<div class="col-xs-3">
-						<a href="#" class="thumbs-down">4 <span class="glyphicon glyphicon-thumbs-down"></span></a>
-					</div>
-
-				</div>
-			</li>
-		</ul>
+		<div class="append-in">
+		</div>
 		<ul class="nav nav-pills nav-ehri nav-ehri-grey nav-justified">
 			<li>
 				<button class="nav-ehri nav-ehri-grey nav-ehri-input">New</button>
