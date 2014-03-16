6960044ff973ce2bf259e73365845358ef87f7ff
Thibault Clérice <leponteineptique@gmail.com>
2014-03-16 12:54:34 +0000

    Fixing Issue #7

--- routes/annotations.php
+++ routes/annotations.php
@@ -2,6 +2,11 @@
 	$app->get('/annotations/', function () use($app)  {
 		$data = array();
 		$data["annotations"] = Annotations::Available(false, true, false);
-		display("./pages/annotations.all.php", $data, array("annotations"), $title = "Annotations");
+		if(isset($_SESSION["user"])) {
+			$extrascripts = array("annotation-connected");
+		} else {
+			$extrascripts = array("annotations");
+		}
+		display("./pages/annotations.all.php", $data, $extrascripts, $title = "Annotations");
 	});
 ?>
--- templates/pages/annotations.all.php
+++ templates/pages/annotations.all.php
@@ -1,28 +1,30 @@
-<form action="/API/annotations/type" class="form form-horizontal" id="new-type" method="POST">
-	<h2>Add annotation type</h2>
 
-	<div class="form-group">
-		<label class="col-sm-2 control-label">Name of the new type</label>
-		<div class="col-sm-10">
-			<input type="text" class="form-control" placeholder="New type legend" name="type_name" />
+<?if(isset($_SESSION["user"])):?>
+	<form action="/API/annotations/type" class="form form-horizontal" id="new-type" method="POST">
+		<h2>Add annotation type</h2>
+
+		<div class="form-group">
+			<label class="col-sm-2 control-label">Name of the new type</label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" placeholder="New type legend" name="type_name" />
+			</div>
 		</div>
-	</div>
-	<div class="form-group">
-		<label class="col-sm-2 control-label">Target of the new type</label>
-		<div class="col-sm-10">
-			<select name="target">
-				<option value="lemma">Lemma</option>
-				<option value="sentence">Sentence</option>
-			</select>
+		<div class="form-group">
+			<label class="col-sm-2 control-label">Target of the new type</label>
+			<div class="col-sm-10">
+				<select name="target">
+					<option value="lemma">Lemma</option>
+					<option value="sentence">Sentence</option>
+				</select>
+			</div>
 		</div>
-	</div>
-	<div class="form-group">
-		<div class="col-sm-offset-2 col-sm-10">
-			<button type="submit" class="btn btn-default">Save</button>
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+				<button type="submit" class="btn btn-default">Save</button>
+			</div>
 		</div>
-	</div>
 </form>
-
+<?endif;?>
 <hr />
 <h2>Annotations available</h2>
 <table class="table table-hover table-bordered" id="annotations-list">
@@ -43,7 +45,9 @@
 						<?foreach($type["options"] as &$value):?>
 							<li><?=$value["text"];?></li>
 						<?endforeach;?>
-						<li class="new-value-line"><a class="add-value" data-target="<?=$type["id"];?>" href="#"><span class="glyphicon glyphicon-plus-sign"></span></a></li>
+						<?if(isset($_SESSION["user"])):?>
+							<li class="new-value-line"><a class="add-value" data-target="<?=$type["id"];?>" href="#"><span class="glyphicon glyphicon-plus-sign"></span></a></li>
+						<?endif;?>
 					</ul>
 				</td>
 			</tr>
