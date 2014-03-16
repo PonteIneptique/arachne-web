6fb046b9f4bb286226169657102ff43858ed2d43
Thibault Clérice <leponteineptique@gmail.com>
2014-03-16 11:48:53 +0000

    Small cleaning in lemma view

--- templates/pages/lemma.php
+++ templates/pages/lemma.php
@@ -2,7 +2,6 @@
 	<article class="col-md-6" data-target="<?=$lemma["uid"]?>" id="lemma">
 		<div  style="padding:10px;">
 			<h1><?=$lemma["lemma"]?></h1>
-			<h2>Polarity</h2>
 			<h2>Annotation</h2>
 
 			<? foreach ($annotations as &$anno): ?>
@@ -10,7 +9,7 @@
 				<div class="col-md-8"><?=$anno["text_type"];?> : <?=$anno["text_value"];?></div>
 				<div class="col-md-4">
 
-					<?if(isset($_SESSION["user"])) { $CL = "a";} else { $CL = "span"; } ?>
+					<?if(isset($_SESSION["user"])) { $CL = "span";} else { $CL = "span"; } ?>
 					<<?=$CL;?> class="annotations-thumbs-up" data-target="<?=$anno["id_annotation"];?>" href="#">
 						<? if($anno["votes"] > 0 ) { echo $anno["votes"]; } else { echo "0"; } ?> <span class="glyphicon glyphicon-thumbs-up"></span>
 					</<?=$CL;?>> 
@@ -27,7 +26,7 @@
 						<li><?=$form["text_form"];?> <small>( <?=$form["sentences"];?> Sentence(s),  <?=$form["votes"];?> Vote(s) )</small></li>
 					<? endforeach; ?>
 				</ul>
-				
+
 			<h2>Sentences</h2>
 			<ul class="list-unstyled sentence-list">
 				<?foreach($sentences as &$sentence):?>
