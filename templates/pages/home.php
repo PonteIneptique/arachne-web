1b60c2d4551d48216ea814105e47957ae5102395
Thibault Clérice <leponteineptique@gmail.com>
2014-03-16 20:54:01 +0000

    Added some explanation (#2)

--- templates/pages/home.php
+++ templates/pages/home.php
@@ -2,3 +2,10 @@
 
 <h2>What is Lasciva Roma ?</h2>
 <p>This whole project aims to evaluate the Latin lexical field of sexuality. One of the problems we have doing that in onomastic is our unability to have a wider point of view.</p>
+
+<h2>What is my role ?</h2>
+<p>Your mission in this empire is to annotate forms according to your knowledge in Latin culture or grammar ! As the results shown here are issued by automatic functionnalities, many of them are unsure.</p>
+<p>So for them, we need you're citizen power : the <b>vote</b>.</p>
+<p>But we also don't have a lot of information about each item, so maybe you could use your rhetor power : the <b>annotation</b>.</p>
+<p>As we are kind of democratic hipster (We don't count Greek as our predecessor. That would be wrong.), you can actually propose new annotation type and value through the Annotation link on the top</p>
+<p>Of course you can only browse the result, but where is the fun in it ?</p>
