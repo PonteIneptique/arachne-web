d3036535c27d7c91d18392c4e3cb361c9f4624b0
Thibault Clérice <leponteineptique@gmail.com>
2014-03-19 19:30:33 +0000

    Better interface

--- templates/_main.php
+++ templates/_main.php
@@ -31,47 +31,34 @@
 				<div class="container">
 					<div class="row">
 						<div class="account">
-							<?if(isset($_SESSION["user"])):?>
-								<div class="login-link">
-									<a href="/account/profile">Profile</a>
-									<a href="/account/history">History</a>
-									<a href="/account/signout">Sign out</a>
-								</div>
-							<?else:?>
-								<a href="/account/login" class="login-link">Login</a>
-							<?endif;?>
+							<div class="login-link">
+								<?if(isset($_SESSION["user"])):?>
+										<a href="/account/profile">Profile</a>
+										<a href="/account/history">History</a>
+										<a href="/account/signout">Sign out</a>
+
+										<table class="gamification pull-right">
+											<tr>
+												<td valign="middle" class="image-container">
+													<a class="image-container"><?=$_SESSION["user"]["game"]["image"];?></a>
+												</td>
+												<td valign="middle" class="image-container">
+													<input type="text" class="dial" data-width="50" data-height="50" data-min="0" value="<?=$_SESSION["actions"]["user"];?>" data-max="<?=$_SESSION["actions"]["total"]?>" data-readOnly="true">
+												</td>
+											</tr>
+										</table>
+								<?else:?>
+									<a href="/account/login" class="login-link">Login</a>
+								<?endif;?>
+							</div>
 						</div>
 						<div class="badges">
-							<?if(isset($_SESSION["user"])):?>
-								<table class="gamification pull-right">
-									<tr>
-										<td valign="middle" style="color:white;">
-											Your Stats
-										</td>
-										<td valign="middle" class="image-container">
-											<a class="image-container"><?=$_SESSION["user"]["game"]["image"];?></a>
-										</td>
-										<td valign="middle" class="image-container">
-											<input type="text" class="dial" data-width="50" data-height="50" data-min="0" value="<?=$_SESSION["actions"]["user"];?>" data-max="<?=$_SESSION["actions"]["total"]?>" data-readOnly="true">
-										</td>
-									</tr>
-								</table>
-							<?endif;?>
-							<table class="gamification">
-								<tr>
-									<td valign="middle" style="color:white;">
-										Shortcuts
-									</td>
-									<td valign="middle" class="image-container">
-										<a class="image-container" href="/sentence/random" title="Random sentence"><img src="/assets/images/random.png" alt="Random sentence" /></a>
-									</td>
-									<?if(isset($_SESSION["user"])):?>
-									<td valign="middle" class="image-container">
-										<a class="image-container" href="/sentence/last" title="Last sentence"><img src="/assets/images/last.png" alt="Last sentence" /></a>
-									</td>
-									<?endif;?>
-								</tr>
-							</table>
+							<div class="shortcuts">
+								<a href="/sentence/random" title="Random sentence">Random sentence</a>
+								<?if(isset($_SESSION["user"])):?>
+									<a href="/sentence/last" title="Last sentence">Last sentence</a>
+								<?endif;?>
+							</div>
 							<!--<h1 style="color:white; font-variant: small-caps; font-size:30px; font-weight: bold; margin-bottom:20px;"><?=$title;?></h1>
 							-->
 						</div>
--- templates/pages/home.php
+++ templates/pages/home.php
@@ -3,11 +3,11 @@
 
 <div class="text-center">
 	<?if(isset($_SESSION["user"])):?>
-	<a class="btn btn-success" href="/sentence/<?=Sentence::Random();?>" >
+	<a  style="margin-top: 20px; margin-bottom: 20px;" class="btn btn-success" href="/sentence/<?=Sentence::Random();?>" >
 		Start annotating
 	</a>
 	<?else:?>
-	<a class="btn btn-success" href="/account/login" >
+	<a style="margin-top: 20px; margin-bottom: 20px;" class="btn btn-success" href="/account/login" >
 		Sign-up or login before starting contributing
 	</a>
 	<?endif;?>
@@ -17,44 +17,38 @@
 <h2>What is Lasciva Roma ?</h2>
 <p>This whole project aims to annotate the Latin lexical field of sexuality. One of the problems we have doing that in onomastic is our unability to have a wider point of view, this tries to fill the hole.</p>
 
-
-<h2>What is my role ?</h2>
 	<div class="row">
 		<div class="col-sm-4">
 			<div class="thumbnail" style="height:100%; border:0; margin:0 auto;">
-				<img src="/assets/images/placeholder.svg" alt="...">
+				<img class="img-circle" src="/assets/images/lemmatization.png" alt="...">
 				<div class="caption">
 					<h3>Lemmatize</h3>
 					<p>Lemmatize name of persons and place by clicking on word in sentence</p>
+					<p class="text-muted"><span class="glyphicon glyphicon-question-sign"></span> Words in black have no lemma attached to them. They are your targets !</p>
 				</div>
 			</div>
 		</div>
 		<div class="col-sm-4">
 			<div class="thumbnail" style="height:100%; border:0; margin:0 auto;">
-				<img src="/assets/images/placeholder.svg" alt="...">
+				<img src="/assets/images/annotate.png" class="img-circle" alt="...">
 				<div class="caption">
 					<h3>Annotate</h3>
 					<p>Propose new annotation type or directly annotate item</p>
+					<p class="text-muted"><span class="glyphicon glyphicon-question-sign"></span> Click "Annotation" in the menu bar to see the list of available annotations and propose your own. Browse sentence and click lemma to annotate them !</p>
 				</div>
 			</div>
 		</div>
 		<div class="col-sm-4">
 			<div class="thumbnail" style="height:100%; border:0; margin:0 auto;">
-				<img src="/assets/images/placeholder.svg" alt="...">
+				<img src="/assets/images/rate.png" class="img-circle" alt="...">
 				<div class="caption">
 					<h3>Rate</h3>
-					<p>For each annotation and lemmatization, place your vote to tell if you think they are right or not</p>
+					<p>For each annotation and lemmatization, place your vote to tell if you think they are right or not by clicking <span class="glyphicon glyphicon-thumbs-up"></span> or <span class="glyphicon glyphicon-thumbs-down"></span></p>
+					<p class="text-muted"><span class="glyphicon glyphicon-question-sign"></span> In sentence, words in red have not enough votes and green have enough.</p>
 				</div>
 			</div>
 		</div>
 	</div>
-
-
-<p>Your mission in this empire is to annotate forms according to your knowledge in Latin culture or grammar ! As the results shown here are issued by automatic functionnalities, many of them are unsure.</p>
-<p>So for them, we need your citizen power : the <b>vote</b>.</p>
-<p>But we also don't have a lot of information about each item, so maybe you could use your rhetor power : the <b>annotation</b>.</p>
-<p>As we are kind of democratic hipster (We don't count Greek as our predecessor. That would be wrong.), you can actually propose new annotation type and value through the Annotation link on the top</p>
-<p>Of course you can only browse the result, but where is the fun in it ?</p>
 
 <div class="text-right">
 	<a href="/about" class="btn btn-warning">Learn more about the project</a>
